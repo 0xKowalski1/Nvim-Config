f4199a9bd2d1178973a67ce9dcca565b864007ec
0xKowalski <0xkowalskiaudit@gmail.com>
2024-05-09 11:36:36 +0100

    Changed keybindings for telescope from ctrl to alt

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -2,11 +2,11 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Find files using Telescope command-line sugar.
-map('n', '<C-p>', '<cmd>Telescope find_files<cr>', opts) -- Find files in cur dir
+map('n', '<A-p>', '<cmd>Telescope find_files<cr>', opts) -- Find files in cur dir
 map('n', '<S-p>', '<cmd>Telescope find_files cwd=~/<cr>', opts) -- Find files in home
 
 -- Live grep using Telescope
-map('n', '<C-f>', '<cmd>Telescope live_grep<cr>', opts) -- Grep cur dir
+map('n', '<A-f>', '<cmd>Telescope live_grep<cr>', opts) -- Grep cur dir
 map('n', '<S-f>', '<cmd>Telescope live_grep cwd=~/<cr>', opts) -- Grep home
 
 -- Ticks! For todo lists
